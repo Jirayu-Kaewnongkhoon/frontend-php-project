d833c33adf9898ae92fd9de1baabd23acfb192d6
JirayuKaewnongkhoon <60050149@kmitl.ac.th>
2020-11-01 23:40:31 +0700

    Edit UI in JobList & JobItem

--- src/components/JobItem.js
+++ src/components/JobItem.js
@@ -1,14 +1,58 @@
-import React from 'react'
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+
+const useStyles = makeStyles(() => ({
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    cardMedia: {
+        paddingTop: '56.25%', // 16:9
+    },
+    cardContent: {
+        flexGrow: 1,
+    }
+}));
 
 function JobItem(props) {
+
+    const classes = useStyles();
+
     return (
-        <div>
-            {`Job Desc : ${props.data.description}`}
-            <img 
-                src={props.data.pre_image_path} 
-                alt='' 
-                width="500px" />
-        </div>
+        <Grid item xs={12} sm={6} md={4}>
+            <Card className={classes.card}>
+                <CardMedia
+                    className={classes.cardMedia}
+                    image={props.data.pre_image_path}
+                    title="Image title"
+                />
+                <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {props.data.description}
+                    </Typography>
+                    <Typography>
+                        This is a media card. You can use this section to describe the content.
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        View
+                    </Button>
+                    <Button size="small" color="primary">
+                        Assign
+                    </Button>
+                </CardActions>
+            </Card>
+        </Grid>
     )
 }
 
--- src/pages/JobList.js
+++ src/pages/JobList.js
@@ -1,21 +1,52 @@
 import React, { useEffect } from 'react'
 import { authenticationService } from '../services/authenticationService';
-import { Link } from 'react-router-dom';
-// import { createBrowserHistory } from 'history';
+import { createBrowserHistory } from 'history';
 import axios from 'axios';
 import JobItem from '../components/JobItem';
 
+// job list template
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+// import Link from '@material-ui/core/Link';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+        marginTop: theme.spacing(4),
+    },
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+    },
+}));
+
 function JobList() {
 
-    const currentUser = authenticationService.currentUserValue;
-    const [jobList, setJobList] = React.useState([]);
-    // const history = createBrowserHistory({ forceRefresh: true });
+    const classes = useStyles();
+    const history = createBrowserHistory({ forceRefresh: true });
 
-    // useEffect(() => {
-    //     if (!currentUser) {
-    //         history.push('/login');
-    //     }
-    // }, [])
+    const [jobList, setJobList] = React.useState([]);
 
     useEffect(() => {
         axios.get("https://database-php-project.000webhostapp.com/getJobRequest.php")
@@ -27,25 +58,67 @@ function JobList() {
     }, [])
 
     return (
-        <div>
-            <div>
-                <h2>Name : {currentUser.user_name}</h2>
-                <h2>Your role is : "{currentUser.role_name}"</h2>
-                <Link to={'/login'} >
-                    <button
-                        type="button"
-                        onClick={() => authenticationService.logout()}
-                    >
-                        Log out
-                    </button>
-                </Link>
+        <React.Fragment>
+            <CssBaseline />
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" className={classes.title}>
+                            Job List
+                        </Typography>
+                        <Button
+                            color="inherit"
+                            onClick={() => {
+                                authenticationService.logout();
+                                history.push('/login');
+                            }}
+                        >
+                            Logout
+                        </Button>
+                    </Toolbar>
+                </AppBar>
             </div>
-
-            <hr />
-
-            JobList page <br /><br />
-            {jobList.map((job, index) => <JobItem key={index} data={job} />)}
-        </div>
+            <main>
+                {/* Hero unit */}
+                <div className={classes.heroContent}>
+                    <Container maxWidth="sm">
+                        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                            Album layout
+                        </Typography>
+                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                            Something short and leading about the collection below—its contents, the creator, etc.
+                            Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+                            entirely.
+                        </Typography>
+                        <div className={classes.heroButtons}>
+                            <Grid container spacing={2} justify="center">
+                                <Grid item>
+                                    <Button variant="contained" color="primary">
+                                        Main call to action
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="outlined" color="primary">
+                                        Secondary action
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </Container>
+                </div>
+                <Container className={classes.cardGrid} maxWidth="md">
+                    {/* End hero unit */}
+                    <Grid container spacing={4}>
+                        {jobList.map((job, index) => (
+                            <JobItem key={index} data={job} />
+                        ))}
+                    </Grid>
+                </Container>
+            </main>
+        </React.Fragment>
     )
 }
 
